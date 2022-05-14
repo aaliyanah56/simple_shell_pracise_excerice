f2e71915fb65a9fff25009c659491162627454e8
aaliyanah56 <rahmaadannoor@gmail.com>
2022-05-14 14:26:39 +0300

    .\_fork.c

---
+++ _fork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - fork example
+*
+* Return: Always 0.
+* gcc -Wall -pedantic -Werror -Wextra -std=gnu89 fork.c -o fork && ./fork
+*/ 
+int main(void);
+{
+pid_t my_pid;
+pid_t child_pid;
+
+child_pid = fork();
+if (child_pid == -1)
+{
+printf("(%u)!\n", my_pid);
+}
+else
+{
+printf("(%u) %u\n", my_pid, child_pid);
+}
+return (0);
+}
