b1d18907b0f7164889ea57871bafa148519c9985
aaliyanah56 <rahmaadannoor@gmail.com>
2022-05-14 15:28:29 +0300

    .\strtok.c

--- strtok.c
+++ strtok.c
@@ -2,28 +2,25 @@
 #include <string.h>
 
 /**
- * main - splits a string and returns an array of each word of the string
- *
- * Return: 0
- */
-int main(void)
-{
-int j, i = 0;
-char str[80] = "hello world goodbye 510";
-char *delim = " ";
-char *res;
-char *words[5];
+* main - splits a string and returns an array of each word of the string
+*
+* Return: 0
+*/
 
-/* get first token */
-words[i++] = strtok(str, delim);
-
-/* get all other tokens */
-while((res = strtok(NULL, delim)))
-words[i++] = res;
-
-/* print all tokens */
-for (j = 0; j < i; j++)
-printf("%s\n", words[j]);
-
-return (0);
+int main () {
+char str[80] = "hello world goodbye";
+const char s[2] = "-";
+char *token;
+   
+/* get the first token */
+token = strtok(str, s);
+   
+/* walk through other tokens */
+while( token != NULL ) {
+printf( " %s\n", token );
+    
+token = strtok(NULL, s);
+}
+   
+return(0);
 }
