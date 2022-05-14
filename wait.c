204fe17fde2390d4a33f87b324cd3cfd550c82c9
aaliyanah56 <rahmaadannoor@gmail.com>
2022-05-14 15:16:20 +0300

    .\wait.c

---
+++ wait.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+* main - fork & wait example
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+pid_t child_pid;
+ppid_t getppid;
+int status;
+
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("Error:");
+return (1);
+}
+if (child_pid == 0)
+{
+printf("child_pid\n", child_pid);
+sleep(3);
+}
+else
+{
+wait(&status);
+printf("ppid_t\n", getpid);
+}
+return (0);
+}
