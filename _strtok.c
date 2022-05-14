a731e9d2b7df92cd6ba479c5b099a40609b86644
aaliyanah56 <rahmaadannoor@gmail.com>
2022-05-14 14:55:23 +0300

    .\_strtok.c

--- _strtok.c
+++ _strtok.c
@@ -2,26 +2,26 @@
 #include<stdlib.h>
 #include <string.h>
 /**
- * main - string without use of strtok
- * 
- * return - 0 
- */
+* main - string without use of strtok
+* 
+* return - 0 
+*/
 
 int main()
 {
 char arr[100];
 int i=0,len=0;
 
-printf("Enter string: ");
-fgets(arr, 100, stdin);
+write("Enter string: ");
+getline(arr, 100, stdin);
 
 len = strlen(arr);
 
 for(i=0; i<len; i++)
 {
-if(arr[i]==' ')  // here in ' ' is the character upon basis of which you want to tokenize in this code there is space
+if(arr[i]=='=')  // here in ' ' is the character upon basis of which you want to tokenize in this code there is space
 {
-arr[i]='\n' ;
+arr[i]='\0' ;
 }
 }
 puts(arr);
